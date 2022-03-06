142884815360689727f2be5a545ef7df800506e4
Habib Santoso <habib.santoso.ft18@mail.umy.ac.id>
2022-03-06 14:12:20 +0700

    connected all variable with layout componen

--- app/src/main/java/com/example/constraint_layout/MainActivity.java
+++ app/src/main/java/com/example/constraint_layout/MainActivity.java
@@ -21,5 +21,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Menghubukan variable btnlogin dengan componen button pada layout
+        btnlogin = findViewById(R.id.btSingin);
+
+        //Menghubukan variable edemail dengan componen editText pada layout
+        edemail = findViewById(R.id.edEmail);
+
+        //Menghubukan variable edpassword dengan componen button pada layout
+        edpassword = findViewById(R.id.edPassword);
     }
 }
