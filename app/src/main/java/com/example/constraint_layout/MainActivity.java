5f862b0340b5441fce61ea1e4ef0dc0452ff3d0d
Habib Santoso <habib.santoso.ft18@mail.umy.ac.id>
2022-03-06 14:04:52 +0700

    adding variable for editText and button

--- app/src/main/java/com/example/constraint_layout/MainActivity.java
+++ app/src/main/java/com/example/constraint_layout/MainActivity.java
@@ -3,8 +3,19 @@ package com.example.constraint_layout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Deklasrasi variable untuk button
+    Button btnlogin;
+
+    //Deklasrasi variable untuk editText
+    EditText edemail, edpassword;
+
+    //Deklasrasi variable untuk menyimpan email dan password
+    String nama, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
