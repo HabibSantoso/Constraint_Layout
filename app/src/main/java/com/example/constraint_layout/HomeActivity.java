5c22ffa5dfcba80391fc11feed0de7d1ad6d5de6
Habib Santoso <habib.santoso.ft18@mail.umy.ac.id>
2022-03-29 07:12:03 +0700

    complate setup homeactivity

--- app/src/main/java/com/example/constraint_layout/HomeActivity.java
+++ app/src/main/java/com/example/constraint_layout/HomeActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -41,10 +44,43 @@ public class HomeActivity extends AppCompatActivity implements PopupMenu.OnMenuI
             classNamaArrayList.add(classNama);
         }
 
+        adapter = new ListViewAdapter(this);
+
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String nama = classNamaArrayList.get(position).getName();
+
+                bundle.putString("a", nama.trim());
+
+                PopupMenu pm = new PopupMenu(getApplicationContext(), view);
+
+                pm.setOnMenuItemClickListener(HomeActivity.this);
+
+                pm.inflate(R.menu.popup_menu);
+
+                pm.show();
+
+            }
+        });
+
     }
 
     @Override
     public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.mnview:
+                intent = new Intent(getApplicationContext(), LihatDataActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+                break;
+            case R.id.mnedit:
+                Toast.makeText(getApplicationContext(), "Ini untuk edit kontak", Toast.LENGTH_LONG).show();
+                break;
+        }
         return false;
     }
 }
